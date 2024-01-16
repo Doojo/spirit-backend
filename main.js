b7b8e7b16c58599bbdbdba66cb8b505e1f0a1b0c
chaitnya23 <chaitnyagiri2306@gmail.com>
2024-01-16 21:59:31 +0530

    updates

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const app = express();
 const cors = require('cors');
 
 app.use(cors());
@@ -11,7 +12,8 @@ app.get('/', (req, res) => {
   res.send({ message: 'Hello from  Backend' });
 });
 
-
+const port = 4000;
+const host = "localhost";
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
