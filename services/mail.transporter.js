b5eca53264cbd2e9cb8f297720d19f89dad0a065
itsgits01 <giteshambre1@gmail.com>
2024-03-18 13:30:44 +0530

    changes

--- services/mail.transporter.js
+++ services/mail.transporter.js
@@ -23,17 +23,4 @@ const mailTransporter = nodemailer.createTransport({
     }
 });
 
-await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-            reject(error);
-        } else {
-            console.log("Server is ready to take our messages");
-            resolve(success);
-        }
-    });
-});
-
 export {mailTransporter, mailOptions};
